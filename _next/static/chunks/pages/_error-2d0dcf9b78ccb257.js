0fe1c28b8717342feb8ef3c67520a34015588a10
metasophiea <metasophiea@gmail.com>
2024-05-02 16:26:02 +0100

    initial commit

---
+++ _next/static/chunks/pages/_error-2d0dcf9b78ccb257.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4820],{8630:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(5867)}])}},function(n){n.O(0,[2888,9774,179],function(){return n(n.s=8630)}),_N_E=n.O()}]);
