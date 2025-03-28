e5b4598f58f1209d6efd2b7654a292ee271ac5b6
metasophiea <metasophiea@gmail.com>
2025-03-28 13:03:01 +0000

    new news article

---
+++ _next/static/vNxq5qhqm1l3LMTsFf9Wr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-92f6498b0927978f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
